f247eda4545abcc9e3d2a9b220ac6a7f329ffa01
tongfu.e <tongfu.e@xtalpi.com>
2024-11-18 13:33:16 +0800

    feat: first commit

---
+++ src/app/components/icons/index.ts
@@ -0,0 +1,13 @@
+/****************************************************************************
+ *  @Copyright 2024 Xtalpi Systems.
+ *  @Author [tongfu.e@xtalpi.com].
+ *  @Date [2024-11-15 13:48:06].
+ *  @Description All animate icons from https://icons.pqoqubbw.dev/.
+ ***************************************************************************/
+
+export * from './src/calendar';
+export * from './src/settings-gear';
+export * from './src/ship';
+export * from './src/user';
+export * from './src/workflow';
+export * from './src/search';
---
+++ src/app/components/operate-panel.tsx
@@ -0,0 +1,55 @@
+/** ---------------------------------------------------------------------------------------------
+ *  @Author [Tongfu.E].
+ *  @Email [etongfu@outlook.com].
+ *  @Date [2024-11-15 11:37:00].
+ *-------------------------------------------------------------------------------------------- */
+import React from "react";
+
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command"
+
+import { WorkflowIcon, UserIcon, CalendarCogIcon, SettingsGearIcon } from "./icons";
+
+const OperatePanel: React.FC = () => {
+  return (
+    <Command>
+      <CommandInput placeholder="Type a command or search..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          <CommandItem>
+            <CalendarCogIcon />
+            <span>Calendar</span>
+          </CommandItem>
+          <CommandItem>
+            <WorkflowIcon />
+            <span>Workflow</span>   
+          </CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading="Settings">
+          <CommandItem>
+            <UserIcon />
+            <span>Profile</span>
+          </CommandItem>
+          <CommandItem>
+            <SettingsGearIcon />
+            <span>Settings</span>
+          </CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  );
+}
+
+export default OperatePanel;
+
