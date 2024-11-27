f21dffaed546a69cf88e202c2a204cd8a5e00e60
tongfu.e <tongfu.e@xtalpi.com>
2024-11-27 15:14:25 +0800

    feat: add Shortcut

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri_plugin_global_shortcut::GlobalShortcutExt;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,9 +9,26 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(desktop)]
+            {
+                use tauri_plugin_global_shortcut::{ Shortcut, Code, Modifiers };
+
+                let ctrl_command_f = Shortcut::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::KeyF);
+                app.handle().plugin(
+                    tauri_plugin_global_shortcut::Builder::new().with_handler(move |_app, shortcut, _| {
+                        println!("{:?}", shortcut);
+                        if shortcut == &ctrl_command_f {
+                            println!("Ctrl + Shift + F Detected!");
+                        }
+                    }).build()
+                )?;
+                app.handle().global_shortcut().register(ctrl_command_f)?;
+            }
+            Ok(())
+        })
         .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_clipboard_manager::init())
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
